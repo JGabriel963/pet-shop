a60c718cd24806b59c4b52df78088cf2bf9dedc0
João Gabriel <joaogabriel9633@gmail.com>
2025-10-22 06:57:00 -0300

    init appointment form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+import AppointmentForm from '@/components/appointment-form/appointment-form';
+import { PeriodSection } from '@/components/period-section';
+import { groupAppointmentByPeriod } from '@/utils/appointment-utils';
+import { APPOINTMENT_DATA } from '@/utils/mock-data';
+
 export default function Home() {
+  const periods = groupAppointmentByPeriod(APPOINTMENT_DATA);
+
   return (
-    <div>
-      <h2>Rocket</h2>
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between mb-8">
+        <div>
+          <div>
+            <h1 className="text-title-size text-content-primary mb-2">
+              Sua agenda
+            </h1>
+            <p className="text-paragraph-medium-size text-content-secondary">
+              Aqui você pode ver todos os clientes e serviços agendados para
+              hoje.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="pb-24 md:pb-0">
+        {periods.map((period, index) => (
+          <PeriodSection period={period} key={index} />
+        ))}
+      </div>
+
+      <div>
+        <AppointmentForm />
+      </div>
     </div>
   );
 }
---
+++ src/components/period-section/period-section.tsx
@@ -0,0 +1,47 @@
+import { AppointmentPeriod } from '@/types/appointment';
+import { Sun, Cloudy, Moon } from 'lucide-react';
+import { AppointmentCard } from '../appointment-card';
+
+const periodIcons = {
+  morning: <Sun className="text-accent-blue" />,
+  afternoon: <Cloudy className="text-accent-orange" />,
+  evening: <Moon className="text-accent-yellow" />,
+};
+
+type PeriodSectionProps = {
+  period: AppointmentPeriod;
+};
+
+export function PeriodSection({ period }: PeriodSectionProps) {
+  return (
+    <section className="mb-8 bg-background-tertiary rounded-xl">
+      <div className="flex items-center px-5 py-3 justify-between border-b border-[#2e2c30]">
+        <div className="flex items-center gap-2">
+          {periodIcons[period.type]}
+          <h2 className="text-label-large-size text-content-primary">
+            {period?.title}
+          </h2>
+        </div>
+        <span className="text-label-large-size text-content-secondary">
+          {period.timeRange}
+        </span>
+      </div>
+
+      {period.appointments.length > 0 ? (
+        <div className="px-5">
+          <div>
+            {period.appointments.map((appointment, index) => (
+              <AppointmentCard
+                appointment={appointment}
+                key={index}
+                isFirstInSection={index === 0}
+              />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <p>Nenhum agendamento para este período.</p>
+      )}
+    </section>
+  );
+}
